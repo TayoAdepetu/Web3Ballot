22cd11d3f620c8e28e8697e4de06bca8ed202cff
Tayo <copyafrica93@gmail.com>
2024-03-09 14:23:56 +0000

    handle DelegateVote

--- src/component/DelegateVote.jsx
+++ src/component/DelegateVote.jsx
@@ -1,25 +1,32 @@
 import { Box, Button, Card, Flex, Text, TextField } from "@radix-ui/themes";
+import { useState } from "react";
+import useGiveDelegateVote from "../hooks/useGiveRightToVote";
 
-const DelegateVote = ({ to, handleDelegate }) => {
-    return (
-        <Card size="2" style={{ width: 425 }}>
-            <Flex gap="" align="center">
-                <Box width={"100%"}>
-                    <Flex direction="column" gap="3">
-                        <label>
-                            <Text as="div" size="2" mb="1" weight="bold">
-                                Delegate&apos;s Address
-                            </Text>
-                            <TextField.Input placeholder="Enter Delegate's Address" />
-                        </label>
-                        <Button onClick={() => handleDelegate(to)}>
-                            Delegate vote
-                        </Button>
-                    </Flex>
-                </Box>
-            </Flex>
-        </Card>
-    );
+const DelegateVote = () => {
+  const [address, setAddress] = useState("");
+
+  const handleDelegate = useGiveDelegateVote(address);
+  return (
+    <Card size="2" style={{ width: 425 }}>
+      <Flex gap="" align="center">
+        <Box width={"100%"}>
+          <Flex direction="column" gap="3">
+            <label>
+              <Text as="div" size="2" mb="1" weight="bold">
+                Delegate&apos;s Address
+              </Text>
+              <TextField.Input
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                placeholder="Enter Delegate's Address"
+              />
+            </label>
+            <Button onClick={() => handleDelegate(to)}>Delegate vote</Button>
+          </Flex>
+        </Box>
+      </Flex>
+    </Card>
+  );
 };
 
 export default DelegateVote;
